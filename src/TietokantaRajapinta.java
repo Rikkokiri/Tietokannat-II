ad731f6845bcb4eafed38642cc65e7b6637874a7
Rikkokiri <pilvi.rajala@gmail.com>
2016-05-18 15:22:34 +0300

    Some addition/deletion methods created.

--- src/TietokantaRajapinta.java
+++ src/TietokantaRajapinta.java
@@ -1,7 +1,44 @@
 
 public interface TietokantaRajapinta {
 
-	//------ LUOMISMETODIT -----------------------
+	/*
+	 * Kaikki metodit 
+	 */
+	
+	//.o.o.o.o.o.o.o.o.o.o.o.o. LUOMIS-, POISTAMIS- JA PÄIVITYSMETODIT .o.o.o.o.o.o.o.o.o.o.o.o.
+	
+	//------ Pelaaja ------------
+	
+	public boolean luoPelaaja(int pelaajanID, String pelaajanNimi, int puhnum, String kotipaikka);
+	
+	public boolean poistaPelaaja(int pelaajanID);
+	
+	public boolean vaihdaPelaajanPuhelinnumero(int pelaajanID, int uusiPuhnum);
+	
+	public boolean vaihdaPelaajanKotipaikka(int pelaajanID, int uusiKotipaikka);
+	
+	//------ Rata ------------
+	
+	/*
+	 * Huom!
+	 * 
+	 * LUOKITUS CHAR(4)
+	 * OSOITE CHAR(40)
+	 */
+	public boolean luoRata(int radanID, String radanLuokitus, int vaylienLkm, String osoite, String ratamestari);
+	
+	
+	
+	//------ Väylä ------------
+	
+	
+	
+	//------ Peli ------------
+	
+	public boolean luoPeli()
+	
+	//.o.o.o.o.o.o.o.o.o.o.o.o. YKSINKERTAISET HAUT .o.o.o.o.o.o.o.o.o.o.o.o.
+	
 	
 	
 }
