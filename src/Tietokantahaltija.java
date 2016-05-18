8fdc5b539b14af3049b28407af931a403d6379de
Odixi <vz4ev4r@gmail.com>
2016-05-18 15:44:40 +0300

    metodien lisäys

--- src/Tietokantahaltija.java
+++ src/Tietokantahaltija.java
@@ -20,6 +20,42 @@ public class Tietokantahaltija implements TietokantaRajapinta {
 		System.out.println("Opened database successfully. Yasss.");
 	}
 	//-------------------------------------------
+
+	@Override
+	public boolean luoPelaaja(int pelaajanID, String pelaajanNimi, int puhnum, String kotipaikka) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean poistaPelaaja(int pelaajanID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean vaihdaPelaajanPuhelinnumero(int pelaajanID, int uusiPuhnum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean vaihdaPelaajanKotipaikka(int pelaajanID, int uusiKotipaikka) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean luoRata(int radanID, String radanLuokitus, int vaylienLkm, String osoite, String ratamestari) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean luoPeli() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 	
 	
